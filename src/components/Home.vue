0fc68088ae5c4da2b224e79870db1df39e2e616b
georgematos <georgemattos@gmail.com>
2020-08-15 10:11:12 -0300

    primeiros contatos com Vue, criação de componentes

---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container fluid>
+    <div class="pills">
+      <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount" />
+    </div>
+  </v-container>
+</template>
+
+<script>
+
+import Pill from './Pill'
+
+export default {
+
+  components: { Pill },
+
+  data: function () {
+    return {
+      groupedWords: [
+        {
+          name: "you",
+          amount: 900,
+        },
+        {
+          name: "he",
+          amount: 488,
+        },
+        {
+          name: "i",
+          amount: 938,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.pills {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+</style>
