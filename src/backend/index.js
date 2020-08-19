d9f31d2cbe9cb3a7b78767153b106c89860f4a5b
georgematos <georgemattos@gmail.com>
2020-08-19 09:47:46 -0300

    processando arquivos #03

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,39 +2,12 @@ const { ipcMain } = require('electron')
 
 const pathsToRows = require('./pathsToRows')
 const prepareData = require('./prepareData')
+const groupWords = require('./groupWords')
 
 ipcMain.on('process-subtitles', (event, paths) => {
 
   pathsToRows(paths)
     .then(rows => prepareData(rows))
-    .then(words => console.log(words))
-    .then(() => {
-      event.reply('process-subtitles', [
-        {
-          name: "you",
-          amount: 900,
-        },
-        {
-          name: "he",
-          amount: 488,
-        },
-        {
-          name: "i",
-          amount: 938,
-        },
-        {
-          name: "love",
-          amount: 302,
-        },
-        {
-          name: "she",
-          amount: 478,
-        },
-        {
-          name: "they",
-          amount: 123,
-        },
-      ])
-  })
-
+    .then(words => groupWords(words))
+    .then(groupedWords => event.reply('process-subtitles', groupedWords))
 })
