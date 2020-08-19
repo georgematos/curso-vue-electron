db6ac9ef339a33bc98045cfce48e24611259e10c
georgematos <georgemattos@gmail.com>
2020-08-19 08:50:28 -0300

    processando arquivos #01

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,31 +1,38 @@
 const { ipcMain } = require('electron')
 
+const pathsToRows = require('./pathsToRows')
+
 ipcMain.on('process-subtitles', (event, paths) => {
-  console.log(paths)
-  event.reply('process-subtitles', [
-    {
-      name: "you",
-      amount: 900,
-    },
-    {
-      name: "he",
-      amount: 488,
-    },
-    {
-      name: "i",
-      amount: 938,
-    },
-    {
-      name: "love",
-      amount: 302,
-    },
-    {
-      name: "she",
-      amount: 478,
-    },
-    {
-      name: "they",
-      amount: 123,
-    },
-  ])
+
+  pathsToRows(paths)
+    .then(rows => console.log(rows))
+    .then(() => {
+      event.reply('process-subtitles', [
+        {
+          name: "you",
+          amount: 900,
+        },
+        {
+          name: "he",
+          amount: 488,
+        },
+        {
+          name: "i",
+          amount: 938,
+        },
+        {
+          name: "love",
+          amount: 302,
+        },
+        {
+          name: "she",
+          amount: 478,
+        },
+        {
+          name: "they",
+          amount: 123,
+        },
+      ])
+  })
+
 })
